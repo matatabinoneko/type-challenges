40c02b9e83405fa94a7f21da4fcde98a40cc943a
matatabinoneko <matatabinoneko0721@gmail.com>
2025-07-13 12:42:05 +0900

    I checked answer of 00898-easy-includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,12 @@
-type Includes<T extends readonly any[], U> = any
+// type Includes<T extends readonly any[], U> = T[number] extends Exclude<T[number], U> ? false : true
+
+// type Includes<T extends readonly any[], U> = [Exclude<(U extends T[number] ? U : never), never>] extends [never] ? false : true
+// type Includes<T extends readonly any[], U> = U extends T[number] ? true : false
+type IsEqual<X, Y> =
+    (<T>() => T extends X ? 1 : 2) extends
+    (<T>() => T extends Y ? 1 : 2) ? true : false
+
+type Includes<T extends readonly unknown[], U> =
+  T extends [infer First, ...infer Rest]
+    ? IsEqual<First, U> extends true ? true : Includes<Rest, U>
+    : false
